d248738abf4c7927435da26c99642e7ce413cb70
acligament <kumiwjp@gmail.com>
2025-12-20 21:05:11 +0900

    Update Burn.py

--- Burn.py
+++ Burn.py
@@ -14,7 +14,7 @@ subprocess.run([
     "ffmpeg",
     "-i", "webinar.mp4",
     "-vf",
-    f"subtitles={subtitle}:force_style='FontName=IPAexGothic,FontSize=18,Outline=1,Alignment=2,MarginV=15'",
+    f"subtitles={subtitle}:force_style='subtitles=audio_ja.srt:force_style='FontName=IPAexGothic,FontScale=0.6,Outline=1,Alignment=2,MarginV=20'",
     "-c:a", "copy",
     "webinar_subtitled_test.mp4"
 ], check=True)
